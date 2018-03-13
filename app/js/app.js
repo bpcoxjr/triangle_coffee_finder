afb04cb3a0256287fef8fe2406433453fe51524e
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2018-03-13 10:52:13 -0400

    testing polyfill

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
   // HTML imports feature detection & polyfill
-  (function() {
+  /*(function() {
     if ('registerElement' in document && 'import' in document.createElement('link') && 'content' in document.createElement('template')) {
     } else {
       var e = document.createElement('script');
       e.src = '/bower_components/webcomponentsjs/webcomponents-lite.js';
       document.body.appendChild(e);
     }
-  })();
+  })();*/
 
   /*** loading animations ***/
   setTimeout(function() {
